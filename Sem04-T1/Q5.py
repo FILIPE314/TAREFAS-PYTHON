724f8f9e0e73572fdfe05aa61bdc001d2514a09c
FILIPE314 <161400255+FILIPE314@users.noreply.github.com>
2024-03-18 00:21:55 -0300

    TAREFAS-PYTHON

--- Sem04-T1/Q5.py
+++ Sem04-T1/Q5.py
@@ -2,19 +2,19 @@
 altura = float(input('Se você está reformando sua casa e quer saber a área do piso, a área das paredes e o volume do cômodo, digite aqui a altura do cômodo:'))
 #Armazenando dados em uma variável e imprimindo uma mensagem para o usuário
 comprimento = float(input('Agora o comprimento do cômodo:'))
-#Armazenando dados em uma variável e imprimindo uma mensagem para o usuário.
-largura = float(input('A largura do cômod.o:'))
+#Armazenando dados em uma variável e imprimindo uma mensagem para o usuário
+largura = float(input('A largura do cômodo:'))
 #Calculando a área do piso
 areadopiso = comprimento*largura
-#Calculando o volume do cômodo.
+#Calculando o volume do cômodo
 volume = altura*comprimento*largura
-#Calculando a área da parede.
+#Calculando a área da parede
 areadaparede = 2*comprimento*altura+2*largura*altura
-#Transformando para números inteiros.
+#Transformando para números inteiros
 areadopiso = int(areadopiso)
 areadaparede = int(areadaparede)
 volume = int(volume)
-#Imprimindo o resultado na tela.
+#Imprimindo o resultado na tela
 print('Área do piso:',areadopiso)
 print('Volume do cômodo:',volume)
 print('Área da parede:',areadaparede)
